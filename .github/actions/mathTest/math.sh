e060887e4a73e776c4045a90171cf03b582934d2
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-05-03 00:47:37 -0400

    Updated math test
    Added answer to math test and improved script to check for variables and output pass/fail

--- .github/actions/mathTest/math.sh
+++ .github/actions/mathTest/math.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
-echo off
-echo "Running test with parameters:"
-echo "$OPERAND1"
-echo "$OPERAND2"
-echo ""
-set -a diff=$OPERAND2-$OPERAND1
-echo "Difference: $diff"
+if [ -z ${OPERAND1+x} ]; 
+then 
+   echo "OPERAND1 is unset";
+   exit 1
+fi
+
+if [ -z ${OPERAND2+x} ]; 
+then 
+   echo "OPERAND2 is unset";
+   exit 1
+fi
+
+if [ -z ${ANSWER+x} ]; 
+then 
+   echo "ANSWER is unset";
+   exit 1
+fi
+
+let diff=$OPERAND2-$OPERAND1
+
+if [ $diff = $ANSWER ]
+then
+  echo "Pass $OPERAND2 - $OPERAND1 = $diff"
+else
+  echo "Fail $OPERAND2 - $OPERAND1 = $diff <> $ANSWER"
+  exit 1
+fi
