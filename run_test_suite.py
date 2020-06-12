e11964a8a4a98b431ba8d8e8aa274be6197339ae
landin3thorsted <landin3thorsted@gmail.com>
2020-06-12 14:41:53 -0400

    add doc strings

--- run_test_suite.py
+++ run_test_suite.py
@@ -22,16 +22,22 @@ import sys
 import pytest
 
 def run_all():
+    """Runs all tests
+    """
     print("Running all tests...")
     pytest.main(['-v', 'app/tests', '--cov-report', 'term-missing', '--cov=app/'])
     pytest.validate(['-v', 'app/tests', '--cov-report', 'term-missing', '--cov=app/'])
 
 def run_coverage_only():
+    """Running coverage report
+    """
     print("Running coverage report...")
     pytest.main(['--cov-report', 'term-missing', '--cov=app/', 'app/tests'])
     pytest.validate(['--cov-report', 'term-missing', '--cov=app/', 'app/tests'])
 
 def run_generative_only():
+    """Runs generative tests
+    """
     print("Running generative testing...")
     pytest.main(['-v', 'app/tests', '--hypothesis-show-statistics', '-k', 'generative'])
     pytest.validate(['-v', 'app/tests', '--hypothesis-show-statistics', '-k', 'generative'])
@@ -39,6 +45,8 @@ def run_generative_only():
 
 
 def main():
+    """Sets up and kicks off all tests
+    """
     parser = argparse.ArgumentParser(
         description="A command line-tool to manage the project.")
     parser.add_argument(
