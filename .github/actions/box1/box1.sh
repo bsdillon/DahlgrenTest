ef3c1decdd13afcc9973e42b32dab0dc4fdf6be4
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-06-23 13:46:29 -0400

    Fix YML and bash
    I fixed two syntax issues in YML and bash syntax for variable setting.

--- .github/actions/box1/box1.sh
+++ .github/actions/box1/box1.sh
@@ -2,12 +2,12 @@
 
 echo "box 1 executing"
 
-ip2 = "0.0.0.0"
+ip2="0.0.0.0"
 input="IP2.txt"
 while IFS= read -r line
 do
   echo "IP2: $line"
-  ip2 = "$line"
+  ip2="$line"
 done < "$input"
 
 ping -c 1 "$ip2"
--- .github/actions/box2/box2.sh
+++ .github/actions/box2/box2.sh
@@ -2,13 +2,13 @@
 
 echo "box 2 executing"
 
-ip1 = "0.0.0.0"
+ip1="0.0.0.0"
 echo "box 1 IP addresses:"
 input="IP1.txt"
 while IFS= read -r line
 do
   echo "IP1: $line"
-  ip1 = "$line"
+  ip1="$line"
 done < "$input"
 
 ping -c 1 "$ip1"
