3b20d3f7f5b081e31544262fe4c5e297099080f4
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-06-23 16:24:03 -0400

    Fixing some code inspector problems.

--- examples/twobox/actions/box1/box1.sh
+++ examples/twobox/actions/box1/box1.sh
@@ -6,8 +6,8 @@ ip2="0.0.0.0"
 input="IP2.txt"
 while IFS= read -r line
 do
-  echo "IP2: $line"
   ip2="$line"
+  echo "IP2: $ip2"
 done < "$input"
 
 echo "box 1 done"
--- examples/twobox/actions/box2/box2.sh
+++ examples/twobox/actions/box2/box2.sh
@@ -7,8 +7,8 @@ echo "box 1 IP addresses:"
 input="IP1.txt"
 while IFS= read -r line
 do
-  echo "IP1: $line"
   ip1="$line"
+  echo "IP1: $ip1"
 done < "$input"
 
 echo "box 2 done"
