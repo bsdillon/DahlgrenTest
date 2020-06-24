aaeee31e2d25d0b280d6192ef64816894a5f6000
kkarp9 <kkarp9@vt.edu>
2020-06-24 15:23:45 -0400

    Update ProjectHealth.py

--- .github/workflows/ProjectHealth.py
+++ .github/workflows/ProjectHealth.py
@@ -10,7 +10,6 @@ count_list tracks the total number of commits per week (of 52 total)
 the last index should indicate the most recent week since the
 creation of the repos or a year past its creation
 '''
-print("Weekly commit count:",
-        str(count_list[len(count_list) - 1]))
+print("Weekly commit count:", str(count_list[len(count_list) - 1]))
 print("Updated")
 reader.close
