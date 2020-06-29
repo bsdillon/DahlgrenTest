f5796f8caef98e4de279ffc52af7521161b0d904
kkarp9 <kkarp9@vt.edu>
2020-06-29 13:12:51 -0400

    Final adjustments to branch, CodeInspector fixes

--- .github/workflows/Project_Health.py
+++ .github/workflows/Project_Health.py
@@ -11,14 +11,14 @@ reader = open(file)
 json_array = json.load(reader)
 count_list = json_array['all']
 count = len(count_list) - 1
-day_count = 0
+DAY_COUNT = 0
 log = {}
 today = datetime.date.today()
 while count >= 0:
-    if(count_list[count]!=0):
-        n_date = str(today - timedelta(days=day_count))
-        log.update( {n_date : count_list[count]})
-        day_count = day_count + 7
+    if count_list[count] != 0:
+        N_DATE = str(today - timedelta(days=DAY_COUNT))
+        log.update({N_DATE : count_list[count]})
+        DAY_COUNT = DAY_COUNT + 7
     count = count - 1
 with open(os.path.join('./html/', 'log.js'), 'w') as out_file:
     out_file.write('var graph = %s;' % json.dumps(log))
@@ -28,6 +28,6 @@ count_list tracks the total number of commits per week (of 52 total)
 the last index should indicate the most recent week since the
 creation of the repos or a year past its creation
 '''
-print("Weekly commit count:", str(count_list[len(count_list) - 1]))
-print("Updated again 5")
+#print("Weekly commit count:", str(count_list[len(count_list) - 1]))
+#print("Updated again 5")
 reader.close
