1d62227c9e8cb1fcf55db5c9177ca266123829a8
kkarp9 <kkarp9@vt.edu>
2020-06-29 13:22:53 -0400

    Lint and CI fix

--- .github/workflows/Project_Health.py
+++ .github/workflows/Project_Health.py
@@ -1,10 +1,14 @@
 import json
-import sys
-import os
 import datetime
+import os
+import sys
 from datetime import timedelta
 
-
+'''
+count_list tracks the total number of commits per week (of 52 total)
+the last index should indicate the most recent week since the
+creation of the repos or a year past its creation
+'''
 
 file = sys.argv[1]
 reader = open(file)
@@ -17,17 +21,10 @@ today = datetime.date.today()
 while count >= 0:
     if count_list[count] != 0:
         N_DATE = str(today - timedelta(days=DAY_COUNT))
-        log.update({N_DATE : count_list[count]})
+        log.update({N_DATE: count_list[count]})
         DAY_COUNT = DAY_COUNT + 7
     count = count - 1
 with open(os.path.join('./html/', 'log.js'), 'w') as out_file:
     out_file.write('var graph = %s;' % json.dumps(log))
 
-'''
-count_list tracks the total number of commits per week (of 52 total)
-the last index should indicate the most recent week since the
-creation of the repos or a year past its creation
-'''
-#print("Weekly commit count:", str(count_list[len(count_list) - 1]))
-#print("Updated again 5")
-reader.close
+reader.close()
