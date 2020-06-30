68a3342f73e5cf4520da101ef13c9cbd9f149476
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-06-29 22:11:20 -0400

    fixing write statement

--- .github/ciPython/ymlCommitUpdater.py
+++ .github/ciPython/ymlCommitUpdater.py
@@ -8,10 +8,10 @@ datafile = sys.argv[1]
 outputfile = sys.argv[2]
 out_file = open(outputfile, 'w')
 
-out_file.write("Parsing ",datafile)
-out_file.write("Appending ",datafile)
+out_file.write("Parsing "+datafile)
+out_file.write("Appending "+datafile)
 reader = open(datafile)
 json_array = json.load(reader)
 for key, value in json_array.iteritems() :
-    out_file.write(key, value)
+    out_file.write(key+": "+value)
 reader.close()
