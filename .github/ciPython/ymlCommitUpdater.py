1974c7e546c2187dc1550818de19000505d3ab95
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-06-29 21:13:37 -0400

    Changing the python script to ouput data

--- .github/ciPython/ymlCommitUpdater.py
+++ .github/ciPython/ymlCommitUpdater.py
@@ -6,24 +6,12 @@ from datetime import timedelta
 
 datafile = sys.argv[1]
 outputfile = sys.argv[2]
-print ("Parsing ",datafile)
-print ("Appending ",datafile)
+out_file = open(outputfile, 'w')
+
+out_file.write("Parsing ",datafile)
+out_file.write("Appending ",datafile)
 reader = open(datafile)
 json_array = json.load(reader)
-count_list = json_array['all']
-print ("JSON count",count_list)
-count = len(count_list) - 1
-DAY_COUNT = 0
-log = {}
-today = datetime.date.today()
-
-while count >= 0:
-    if count_list[count] != 0:
-        N_DATE = str(today - timedelta(days=DAY_COUNT))
-        log.update({N_DATE: count_list[count]})
-        DAY_COUNT = DAY_COUNT + 7
-    count = count - 1
-with open(os.path.join('./html/', 'log.js'), 'w') as out_file:
-    out_file.write('var graph = %s;' % json.dumps(log))
-
+for key, value in json_array.iteritems() :
+    out_file.write(key, value)
 reader.close()
