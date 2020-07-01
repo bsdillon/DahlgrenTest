27b6adefc5bd9b4ad7000c236df688b652626949
kkarp9 <kkarp9@vt.edu>
2020-07-01 16:49:53 -0400

    Daily and total commit workflow
    Also organized ciPython folder and made necessary changes to respective workflows.

---
+++ .github/ciPython/contributor_stats.py
@@ -0,0 +1,56 @@
+import datetime
+import sys
+import json
+import os
+
+# keeps track of total commits for each user
+total = open(os.path.join("./html/", "total_commit.txt"), 'r')
+# keeps daily track of total commits for each user
+daily = open("daily_commit.txt", 'w')
+data_file = sys.argv[1]
+reader = open(data_file)
+json_array = json.load(reader)
+old_commits = {}
+COMMIT_COUNT = 0
+# writes a dictionary of old commits from users
+for line in total:
+    word_list = line.split()
+    if word_list[0] == "Total":
+        COMMIT_COUNT = int(word_list[2])
+    elif word_list[0] == "Developer:":
+        old_commits[word_list[1]] = COMMIT_COUNT
+count = 0
+total.close()
+# updates daily commits
+d = datetime.datetime.today()
+daily.write(d.strftime("%d-%B-%Y"))
+daily.write(" Daily Commits\n\n")
+while count < len(json_array):
+    user = json_array[count]["author"]["login"]
+    if old_commits.get(user) is None:
+        daily.write("Daily Commits: 0")
+    else:
+        daily.write("Daily Commits: ")
+        old_count = old_commits[user]
+        daily_count = json_array[count]["total"] - old_count
+        daily.write(str(daily_count) + "\n")
+    daily.write("Developer: ")
+    daily.write(user + "\n")
+    daily.write("-----------------------------\n")
+    count = count + 1
+count = 0
+# updates total commits
+total = open(os.path.join("./html/", "total_commit.txt"), 'w')
+total.write(d.strftime("%d-%B-%Y"))
+total.write(" Total Commits\n\n")
+while count < len(json_array):
+    total.write("Total Commits: ")
+    total.write(str(json_array[count]["total"]) + "\n")
+    total.write("Developer: ")
+    user = json_array[count]["author"]["login"]
+    total.write(user + "\n")
+    total.write("-----------------------------\n")
+    count = count + 1
+reader.close()
+total.close()
+daily.close()
