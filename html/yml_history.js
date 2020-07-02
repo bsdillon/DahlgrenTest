c812fc75ef985154ff4aa9a85c538bea850cdb00
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-07-02 14:39:28 -0400

    Update yml_history.js

--- html/yml_history.js
+++ html/yml_history.js
@@ -1,3 +1,2 @@
-commit["2020-07-02T18:27:00Z"]={"developer": "bsdillon", "message": "Another round of small changes
-
-to check the python append capability.", "files": [".github/workflows/CodeInspect.yml", ".github/workflows/CodeInspectHourly.yml", ".github/workflows/YmlChangeReport.yml", ".github/workflows/codecov.yml"]}
+var commit = {};
+commit["2020-07-02T18:35:27Z"]={"developer": "bsdillon", "message": "Merge branch 'master' of https://github.com/bsdillon/DahlgrenTest", "files": []}
