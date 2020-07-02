5a1cc5dc944a1608e3b89e01321af6206569eab4
GitHub Action <None>
2020-07-02 17:37:01 +0000

    Auto-push yml change history

--- html/yml_history.js
+++ html/yml_history.js
@@ -1,3 +1,1 @@
-commit["2020-07-02T17:35:03Z"]={"developer": "bsdillon", "message": "Merge pull request #187 from bsdillon/justchecking
-
-Attempting to use CI pipeline to publish test update data.", "files": [".github/workflows/YmlChangeReport.yml"]}
+commit["2020-07-02T17:36:46Z"]={"developer": "bsdillon", "message": "Forcing the script to run", "files": [".github/workflows/YmlChangeReport.yml"]}
