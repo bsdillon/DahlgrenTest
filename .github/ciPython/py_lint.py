b97af7a6f09e8caf00461de2d02d5fded103d82b
kkarp9 <kkarp9@vt.edu>
2020-07-07 14:11:41 -0400

    another test to get a txt file of test results

--- .github/ciPython/py_lint.py
+++ .github/ciPython/py_lint.py
@@ -8,11 +8,11 @@ current = datetime.now()
 dt_string = current.strftime("%m/%d/%Y %H:%M:%S")
 writer = open(os.path.join("./html/cidata/", "py_lint_log.txt"), "a")
 writer.write(str(dt_string))
-writer.write("\n------------------------\n")
+writer.write("\n-------------------------------------------------------\n")
 # copies and write log
 for line in log:
     writer.write(line)
 
-writer.write("------------------------\n")
+writer.write("-------------------------------------------------------\n")
 log.close()
 writer.close()
