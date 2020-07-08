94c6aa67d2184d1fa611ab318a8feea710943eaa
GitHub Action <None>
2020-07-08 13:10:43 +0000

    Auto-push yml change history

--- html/cidata/yml_history.js
+++ html/cidata/yml_history.js
@@ -15,3 +15,4 @@ commit["2020-07-06T22:32:25Z"]={"developer": "bsdillon", "message": "Fixing lint
 commit["2020-07-07T03:29:49Z"]={"developer": "bsdillon", "message": "Repaired mix up::Somehow @kkarp9 made this a nightly run instead of one triggered by a yml update.", "files": [".github/workflows/YmlChangeReport.yml"]};
 commit["2020-07-07T13:45:29Z"]={"developer": "kkarp9", "message": "Merge branch 'master' into KKBranch", "files": [".github/workflows/PythonLint.yml", ".github/workflows/YmlChangeReport.yml"]};
 commit["2020-07-08T13:09:34Z"]={"developer": "bsdillon", "message": "Resolved linting errors", "files": [".github/workflows/issues_report.yml"]};
+commit["2020-07-08T13:10:22Z"]={"developer": "bsdillon", "message": "Fixed lint error", "files": [".github/workflows/contributor.yml"]};
