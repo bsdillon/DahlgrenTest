13dc440d7a01666884dffdab2c8ae216db2fe069
kkarp9 <kkarp9@vt.edu>
2020-07-08 17:08:34 -0400

    another update on test

--- .github/ciPython/change.py
+++ .github/ciPython/change.py
@@ -1,7 +1,12 @@
 import os
 import sys
+from datetime import datetime
 
+current = datetime.now()
+dt_string = current.strftime("%m/%d/%Y %H:%M:%S")
 file = open(os.path.join("./html/cidata/", "change.txt"), "w")
-file.write("Greetings, this is the NEW file.")
+file.write("Greetings, this is the NEW file modified at ")
+file.write(dt_string)
+file.write(".")
 
 file.close()
