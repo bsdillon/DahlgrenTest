c9709f5ec0fe04dc8fcf64ff1fa02c07b78adae0
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-07-08 23:09:11 -0400

    Created sloc counter for automatic data

---
+++ .github/ciPython/slocParser.py
@@ -0,0 +1,27 @@
+import json
+import sys
+import datetime
+
+# JSON data file of commit
+datafile = sys.argv[1]
+reader = open(datafile)
+
+counts = {}
+line = reader.readline()
+while line:
+    vals = line.strip().lower.split(' ');
+    if "." in vals[1]:
+        ext = vals[1].split('.')[1]
+        if ext not in counts:
+            counts[ext]=0;
+        counts[ext] += int(vals[0],10);
+    line = reader.readline()
+reader.close()
+
+# Incremental history file
+outputfile = sys.argv[2]
+out_file = open(outputfile, "a+")
+date = datetime.datetime.now().strftime("%x");
+for key in counts:
+   out_file.write("slocs["+data+"]["+key+"]="+counts[key]+";\n")
+out_file.close()
