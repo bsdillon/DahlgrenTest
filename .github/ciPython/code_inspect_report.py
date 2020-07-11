9e0ec7f58bf0b3b62848c00da933eefe69446572
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-07-11 19:57:24 -0400

    Creating code inspector report
    Fixed a couple of lint errors because of file names with a space. Removed hourly code inspector. Added report data file to store code inspector errors. This version is going to run on every push.

---
+++ .github/ciPython/code_inspect_report.py
@@ -0,0 +1,18 @@
+from sgqlc.endpoint.http import HTTPEndpoint
+
+url = 'https://api.code-inspector.com/graphql'
+headers = {
+    'X-Access-Key': sys.argv[1],
+    'X-Secret-Key': sys.argv[2]
+}
+
+# Incremental history file
+outputfile = sys.argv[3]
+out_file = open(outputfile, "a+")
+
+query = '{user {username} }'
+variables = {}
+
+endpoint = HTTPEndpoint(url, headers)
+data = endpoint(query, variables)
+out_file.write(data)
