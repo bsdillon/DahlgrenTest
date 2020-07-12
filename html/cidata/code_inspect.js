321f88e79580c5bbbe4b05902d56fd56bde84feb
CodeInspect History <>
2020-07-12 17:28:01 +0000

    Auto-push CodeInspect history

--- html/cidata/code_inspect.js
+++ html/cidata/code_inspect.js
@@ -1,2 +1,11 @@
 var ciReports = {};
-username bsdillon
+username bsdillondata Noneerrors [{'message': "Cannot query field 'Analysis' on type 'Query'. Did you mean 'analysis' or 'analysisDiff'? (line 1, column 2):\n{Analysis, Violation}\n ^", 'locations': [{'line': 1, 'column': 2}]}, {'message': "Cannot query field 'Violation' on type 'Query'. (line 1, column 12):\n{Analysis, Violation}\n           ^", 'locations': [{'line': 1, 'column': 12}]}]exception HTTP Error 400: Bad Requeststatus 400headers Date: Sun, 12 Jul 2020 17:28:01 GMT
+Content-Type: application/json
+Content-Length: 368
+Connection: close
+Vary: Origin
+Referrer-Policy: origin-when-cross-origin, strict-origin-when-cross-origin
+X-XSS-Protection: 1; mode=block
+Content-Security-Policy: base-uri 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com https://use.fontawesome.com http://cdn.jsdelivr.net/; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect-cdn.atl-paas.net https://d301sr5gafysq2.cloudfront.net https://ajax.googleapis.com https://connect-cdn https://aui-cdn.atlassian.com https://bitbucket.org https://snap.licdn.com https://analytics.twitter.com http://static.ads-twitter.com https://fonts.gstatic.com https://fonts.googleapis.com https://use.fontawesome.com http://cdn.jsdelivr.net https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com http://bat.bing.com; object-src 'none'; default-src 'self' 'unsafe-inline' https://bitbucket.org https://d301sr5gafysq2.cloudfront.net http://www.youtube.com http://t.co https://fonts.gstatic.com https://fonts.googleapis.com https://use.fontawesome.com http://cdn.jsdelivr.net https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com http://bat.bing.com
+X-Permitted-Cross-Domain-Policies: master-only
+
