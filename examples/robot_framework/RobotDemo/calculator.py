5e69880496ca5b4c1f0b5b22da9dfb450854f700
landin3thorsted <landin3thorsted@gmail.com>
2020-07-14 13:07:49 -0400

    add tests that werent committed for some reason

---
+++ examples/robot_framework/RobotDemo/calculator.py
@@ -0,0 +1,34 @@
+class Calculator(object):
+    BUTTONS = '1234567890+-*/^C='
+
+    def __init__(self):
+        self._expression = ''
+
+    def push(self, button):
+        if button not in self.BUTTONS:
+            raise CalculationError("Invalid button '%s'." % button)
+        if button == '=':
+            self._expression = self._calculate(self._expression)
+        elif button == 'C':
+            self._expression = ''
+        elif button == '/':
+            self._expression += '//'    # Integer division also in Python 3
+            print(self._expression)
+        elif button == '^':
+            self._expression += '^'
+        else:
+            self._expression += button
+        return self._expression
+
+    def _calculate(self, expression):
+        try:
+            print(expression)
+            return str(eval(expression))
+        except SyntaxError:
+            raise CalculationError('Invalid expression.')
+        except ZeroDivisionError:
+            raise CalculationError('Division by zero.')
+
+
+class CalculationError(Exception):
+    pass
