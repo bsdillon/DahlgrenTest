02704dd2cedefd0be1f4883bbc4cc04dd582803d
kkarp9 <49703515+kkarp9@users.noreply.github.com>
2020-07-15 14:08:00 -0400

    Remove empty inputs

--- html/cidata/daily_commits.js
+++ html/cidata/daily_commits.js
@@ -1,9 +1,2 @@
 date["06-July-2020"]={"actions-user": 10, "landin3thorsted": 0, "kkarp9": 28, "bsdillon": 0};
 date["07-July-2020"]={"actions-user": 3, "landin3thorsted": 0, "kkarp9": 3, "bsdillon": 8};
-date["10-July-2020"]={};
-date["11-July-2020"]={};
-date["12-July-2020"]={};
-date["13-July-2020"]={};
-date["15-July-2020"]={};
-date["15-July-2020"]={"actions-user": 0, "landin3thorsted": 0, "kkarp9": 0, "bsdillon": 0};
-date["15-July-2020"]={"actions-user": 0, "landin3thorsted": 0, "kkarp9": 0, "bsdillon": 0};
