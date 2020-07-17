0f8ca5d6ae4297c24cbb4646942fcff4c0d9d825
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-07-16 22:39:10 -0400

    Fixing contributor code

--- .github/ciPython/contributor_stats.py
+++ .github/ciPython/contributor_stats.py
@@ -3,71 +3,22 @@ import json
 import os
 import sys
 
-# keeps track of total commits for each user
-total = open(os.path.join("./html/cidata", "total_commit.txt"), 'r')
-# keeps daily track of total commits for each user
-daily = open("daily_commit.txt", 'w')
-# appends daily commits in javascript
-daily_js = open(os.path.join("./html/cidata", "daily_commits.js"), "a")
-data_file = sys.argv[1]
-reader = open(data_file)
-json_array = json.load(reader)
-old_commits = {}
-COMMIT_COUNT = 0
-# writes a dictionary of old commits from users
-for line in total:
-    word_list = line.split()
-    if len(word_list) > 0:
-        if word_list[0] == "Total":
-            COMMIT_COUNT = int(word_list[2])
-        elif word_list[0] == "Developer:":
-            old_commits[word_list[1]] = COMMIT_COUNT
-COUNT = 0
-total.close()
-# updates daily commits
-d = datetime.datetime.today()
-date = d.strftime("%d-%B-%Y")
-# javacript appending
-daily_js.write("date[\""+date+"\"]={")
-daily.write(date)
-daily.write(" Daily Commits\n\n")
-while COUNT < len(json_array):
-    user = json_array[COUNT]["author"]["login"]
-    if old_commits.get(user) is None:
-        daily.write("Daily Commits: 0\n")
-        # javacript appending
-        daily_js.write("\""+user+"\"" + ": 0")
-        if COUNT != len(json_array) - 1:
-            daily_js.write(", ")
-    else:
-        daily.write("Daily Commits: ")
-        old_count = old_commits[user]
-        daily_count = json_array[COUNT]["total"] - old_count
-        daily.write(str(daily_count) + "\n")
-        # javascript appending
-        daily_js.write("\""+user+"\"" + ": " + str(daily_count))
-        if COUNT != len(json_array) - 1:
-            daily_js.write(", ")
-    daily.write("Developer: ")
-    daily.write(user + "\n")
-    daily.write("-----------------------------\n")
-    COUNT = COUNT + 1
-# javascript appending
-daily_js.write("};\n")
-COUNT = 0
-# updates total commits
-total = open(os.path.join("./html/cidata", "total_commit.txt"), 'w')
-total.write(date)
-total.write(" Total Commits\n\n")
-# writes the total commit count for each user
-while COUNT < len(json_array):
-    total.write("Total Commits: ")
-    total.write(str(json_array[COUNT]["total"]) + "\n")
-    total.write("Developer: ")
-    user = json_array[COUNT]["author"]["login"]
-    total.write(user + "\n")
-    total.write("-----------------------------\n")
-    COUNT = COUNT + 1
-reader.close()
-total.close()
-daily.close()
+# Read all data (from the dawn of time) in and parse
+daily_js = open("./../../html/cidata/daily_commits.js", "w")
+json_array = json.load(open(sys.argv[1]))
+
+count = 0
+dates = []
+while count < len(json_array):
+    userData = json_array[count]
+    userName = userData["author"]["login"]
+    weeks = userData["weeks"]
+    for w in weeks:
+        d = datetime.datetime.fromtimestamp(w["w"])
+        date = d.strftime("%d-%B-%Y")
+        if date not in dates:
+            daily_js.write("contributions[\'"+date+"\']={};\n")
+            dates.append(date)
+        daily_js.write("contributions[\'" + date + "\']."+userName+"="+str(w["c"])+";\n")
+    count += 1
+
