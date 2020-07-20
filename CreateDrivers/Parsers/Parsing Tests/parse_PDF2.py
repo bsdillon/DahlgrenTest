2322633efacf3478cb56e9905e2337eb0c16e6ec
kkarp9 <kkarp9@vt.edu>
2020-07-20 16:36:14 -0400

    Removed artifact upload jobs, testing parsing pdf file

---
+++ CreateDrivers/Parsers/Parsing Tests/parse_PDF2.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from tabula import convert_into
+from io import StringIO
+import os
+import requests
+
+url = 'http://scipp.ucsc.edu/groups/fermi/electronics/mil-std-883.pdf'
+r = requests.get(url)
+
+# downloads pdf by writing the content in another pdf
+with open("reader.pdf", "wb") as pdf:
+    for chunk in r.iter_content():
+        if chunk:
+            pdf.write(chunk)
+pdf.close()
+
+# parsing here
+pdfFile = open("reader.pdf", "rb")
+pdfRead = PdfFileReader(pdfFile)
+
+textPage = pdfRead.getPage(10)
+print("Number of pages in this pdf:", pdfRead.numPages)
+print(textPage.extractText())
+pdfFile.close()
+
+# removes the reader pdf file
+os.remove("reader.pdf")
+print("Done parsing.")
