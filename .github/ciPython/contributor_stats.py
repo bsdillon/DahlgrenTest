8d5531852c3e17dffe1e42bf2e5d87c79a01b4ca
kkarp9 <kkarp9@vt.edu>
2020-07-21 11:02:44 -0400

    Test contributor stats

--- .github/ciPython/contributor_stats.py
+++ .github/ciPython/contributor_stats.py
@@ -3,7 +3,7 @@ import json
 import sys
 
 # Read all data (from the dawn of time) in and parse
-week_js = open("./html/cidata/weekly_commits.js", "w")
+week_js = open("./html/cidata/weekly_commits.js", "a")
 json_array = json.load(open(sys.argv[1]))
 week_js.write("var contributions = {};\n")
 
@@ -15,12 +15,12 @@ while count < len(json_array):
     if USERNAME == "actions-user":
         USERNAME = "GitHubAction"
     weeks = userData["weeks"]
-    for w in weeks:
-        d = datetime.datetime.fromtimestamp(w["w"])
-        date = d.strftime("%d-%B-%Y")
-        if date not in dates:
-            week_js.write("contributions[\'"+date+"\']={};\n")
-            dates.append(date)
-        week_js.write("contributions[\'" + date + "\'].")
-        week_js.write(USERNAME+"="+str(w["c"])+";\n")
+    curr_week = weeks[len(weeks) - 2]
+    d = datetime.datetime.fromtimestamp(curr_week["w"])
+    date = d.strftime("%d-%B-%Y")
+    if date not in dates:
+        week_js.write("contributions[\'"+date+"\']={};\n")
+        dates.append(date)
+    week_js.write("contributions[\'" + date + "\'].")
+    week_js.write(USERNAME+"="+str(curr_week["c"])+";\n")
     count += 1
