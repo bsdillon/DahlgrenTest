9caba121fdf39e503b36173dd4c318325eeec1c1
JennTW <67290296+JennTW@users.noreply.github.com>
2020-07-27 12:47:26 -0400

    Create request.js
    Added a file for the code that sends the different XMLHttprequests in the client side code directory.

---
+++ GUIcode/ClientSideCode/request.js
@@ -0,0 +1,26 @@
+const solution = function () {
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'http://localhost:1337/', true)
+    request.setRequestHeader('math', 'number');
+
+    request.onload = function () {
+        alert(request.response);
+    }
+
+    request.send()
+}
+
+const test = function () {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'http://localhost:1337/', true)
+    request.setRequestHeader('hello', 'world');
+
+    request.onload = function () {
+        alert(request.response);
+    }
+
+    request.send()
+}
