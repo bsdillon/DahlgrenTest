f9bef314f0ebb716077e356b33e4749e71392adf
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-07-28 12:14:16 -0400

    Update request.js

--- GUIcode/ClientSideCode/request.js
+++ GUIcode/ClientSideCode/request.js
@@ -2,25 +2,25 @@ const solution = function () {
 
     var request = new XMLHttpRequest()
 
-    request.open('GET', 'http://localhost:1337/', true)
+    request.open('GET', 'http://localhost:1337/', true);
     request.setRequestHeader('math', 'number');
 
     request.onload = function () {
         alert(request.response);
-    }
+    };
 
-    request.send()
+    request.send();
 }
 
 const test = function () {
     var request = new XMLHttpRequest()
 
-    request.open('GET', 'http://localhost:1337/', true)
+    request.open('GET', 'http://localhost:1337/', true);
     request.setRequestHeader('hello', 'world');
 
     request.onload = function () {
         alert(request.response);
-    }
+    };
 
-    request.send()
+    request.send();
 }
