0bb5476420e63d49c0e96d7584f18feee82b7217
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-08-07 01:13:18 -0400

    Putting up the lastest data on actions

--- InterfaceCode/scripts.js
+++ InterfaceCode/scripts.js
@@ -1,12 +1,12 @@
 const solution = function () {
 
-    var request = new XMLHttpRequest()
+    var request = new XMLHttpRequest();
 
-    request.open('GET', 'http://192.168.86.32:1337', true)
+    request.open('GET', 'http://192.168.86.32:1337', true);
 
     request.onload = function () {
         alert(request.response);
-    }
+    };
 
-    request.send()
+    request.send();
 }
