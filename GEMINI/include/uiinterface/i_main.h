414ca47578e8dc75d608e2f0a92aa8e2b8a97f40
bsdillon <57143227+bsdillon@users.noreply.github.com>
2020-08-17 11:48:44 -0400

    Update i_main.h
    Added todo messages for @cdang5 to work on today

--- GEMINI/include/uiinterface/i_main.h
+++ GEMINI/include/uiinterface/i_main.h
@@ -3,12 +3,20 @@
 
 #include "qobject.h"
 
+//TODO edit to compare with MessageListener for slot/signal functionality
+
 class I_Main : public QObject
 {
     Q_OBJECT
 
 public:
     void show();
+    //public functions to set the callbacks
+void setLogEventCallback (std::function<void(const ???)> cb);
+
+   //public signal function
+   void SignalLogMessage(QString s) { emit LogMessage(s);}
+
 public slots:
     void Status(std::string s);
     void modeSwitchingEnabled(bool enabled);
@@ -17,6 +25,18 @@ public slots:
 signals:
     void LogMessage(QString);
 
+//private functions for callbacks
+    std::function<?????//> myprivateLogEventCallback;
 };
+
+//On CPP side
+
+//set each callback from the public function
+
+void setLogEventCallback (std::function<void(const ???)> cb)
+{
+    myprivateLogEventCallback = cb;
+}
+
 
 #endif // I_MAIN_H
