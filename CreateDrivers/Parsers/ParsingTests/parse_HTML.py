50ce8c9aa6508f532949fd1fd900aaa9a0c62d7a
kkarp9 <kkarp9@vt.edu>
2020-08-20 15:27:18 -0400

    Published code of parsing HTML and WORD

--- CreateDrivers/Parsers/ParsingTests/parse_HTML.py
+++ CreateDrivers/Parsers/ParsingTests/parse_HTML.py
@@ -6,8 +6,8 @@ import requests
 
 url = "http://www.tcpipguide.com/free/t_IPDatagramGeneralFormat.htm"
 # make a GET request to fetch raw HTML content
+
 html_content = requests.get(url).text
-# out = open("html_out.txt", "w")
 soup = BeautifulSoup(html_content, "lxml")
 print(soup.title.text)
 table = soup.find("table", attrs={"border": "3"})
@@ -23,12 +23,9 @@ while count < len(title_list):
     if count != 0 and count % 3 == 0:
         field.append(title_list[count])
     count = count + 1
-print(field)
 print("Number of field names:", len(field))
 df = pd.DataFrame()
 df['Field Names'] = field
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    # more options can be specified also
     print(df)
-# out.close()
