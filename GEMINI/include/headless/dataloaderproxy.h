332e5dccbed58192ff1786b42b149929cb33de5c
Chris Dang <cdang@localhost.localdomain>
2020-08-24 16:20:28 -0400

    New private member

--- GEMINI/include/headless/dataloaderproxy.h
+++ GEMINI/include/headless/dataloaderproxy.h
@@ -2,12 +2,16 @@
 #define DATALOADERPROXY_H
 
 #include "proxies/AbstractProxy.h"
+#include "uiinterface/i_dataloader.h"
 
 class DataLoaderProxy : public QObject
 {
     Q_OBJECT
 public:
     DataLoaderProxy();
+
+private:
+    I_DataLoader* DataLoaderProxy;
 };
 
 #endif // DATALOADERPROXY_H
