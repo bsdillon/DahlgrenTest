77fd1d506b6aecbc820e1c6f70891b98b87a74f9
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-10-26 13:34:41 -0400

    used python -m reindent to fix indentation to 4 spaces per the github test suite

--- sys-stat/build.py
+++ sys-stat/build.py
@@ -3,16 +3,15 @@
 import os, sys, subprocess
 
 def cmd(*args):
-  subprocess.check_call([x for x in args])
+    subprocess.check_call([x for x in args])
 
-if __name__ == '__main__':
-  
-  cmd('cargo', 'build', '--release')
+    if __name__ == '__main__':
 
-  # If the host is not running windows, build
-  # a windows copy as well
-  if not sys.platform in ['win32', 'cygwin']:
-    print('> Attempting to cross-compile for x86_64-pc-windows-gnu systems...')
-    print('> See https://wiki.archlinux.org/index.php/Rust#Cross_compiling for cross-compilation setup.')
-    cmd('cargo', 'build', '--release', '--target=x86_64-pc-windows-gnu')
+        cmd('cargo', 'build', '--release')
 
+    # If the host is not running windows, build
+    # a windows copy as well
+    if not sys.platform in ['win32', 'cygwin']:
+        print('> Attempting to cross-compile for x86_64-pc-windows-gnu systems...')
+        print('> See https://wiki.archlinux.org/index.php/Rust#Cross_compiling for cross-compilation setup.')
+        cmd('cargo', 'build', '--release', '--target=x86_64-pc-windows-gnu')
