1b4778c5cada4fb7676c8ff185b0d7372961dbf2
MooreTylerC <tyler.moore.174@gmail.com>
2020-11-20 12:46:13 -0500

    proper reading/printing of json

--- PGREST/main.py
+++ PGREST/main.py
@@ -1,3 +1,14 @@
 import json
-import yml
+import requests
+import urllib
 
+print("possible address: http://192.168.1.182:3000/")
+print("enter address: ")
+addr = input()
+
+print("connecting to: " + addr + "...")
+
+with urllib.request.urlopen(addr) as url:
+    data = json.loads(url.read().decode())
+    print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+    
