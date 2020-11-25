acf4f629a0618c8db02813ca73350caf2fcc14b9
MooreTylerC <tyler.moore.174@gmail.com>
2020-11-25 12:43:09 -0500

    initial experimentation with GUI elements

--- PGREST/main.py
+++ PGREST/main.py
@@ -1,14 +1,42 @@
 import json
 import requests
+import tkinter as tk
 import urllib
 
-print("possible address: http://192.168.1.182:3000/")
-print("enter address: ")
-addr = input()
 
-print("connecting to: " + addr + "...")
+root = tk.Tk()
+# setting the window's size
+root.geometry("480x240")
 
-with urllib.request.urlopen(addr) as url:
-    data = json.loads(url.read().decode())
-    print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
-    
+#declaring string for storing addr
+addr_var=tk.StringVar()
+
+def get_api():
+
+    addr=addr_entry.get()
+    print("connecting to: " + addr + "...")
+
+    with urllib.request.urlopen(addr) as url:
+        data = json.loads(url.read().decode())
+        print(type(data))
+        # print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))) # data dump
+
+        # 'data' object is a python dictionary of dictionaries
+        for item in data['paths']:
+            print(item)
+
+
+tk.Label(root, text="URI to connect to").grid(row=0)
+
+addr_entry = tk.Entry(root, textvariable = addr_var, width=32)
+addr_entry.grid(row=0, column=1)
+
+# tk.Button(root, text="Connect", command=( get_api() )).grid(row=1, column=1)
+
+root.mainloop()
+
+
+
+
+
+
