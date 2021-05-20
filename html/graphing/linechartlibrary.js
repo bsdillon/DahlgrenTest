5ff2f7701492aa9b8551f6e17ea00cad55d74d88
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-05-20 14:39:56 -0400

    Fix Graph Issue
    Added window.chartColors object to ensure that the createAllStyle function can run to generate the graph.

--- html/graphing/linechartlibrary.js
+++ html/graphing/linechartlibrary.js
@@ -211,7 +211,15 @@ function createAllStyle(config, seriesName, linedata, random, fill, group, cat)
   {
     fillStyle="start";
   }
-
+  window.chartColors = {
+        red: 'rgb(255, 99, 132)',
+        orange: 'rgb(255, 159, 64)',
+        yellow: 'rgb(255, 205, 86)',
+        green: 'rgb(75, 192, 192)',
+        blue: 'rgb(54, 162, 235)',
+        purple: 'rgb(153, 102, 255)',
+        grey: 'rgb(231,233,237)'
+      };
   var colorNames = Object.keys(window.chartColors);
   var colorName = colorNames[lineStyleIndex % colorNames.length];
   var c = window.chartColors[colorName];
