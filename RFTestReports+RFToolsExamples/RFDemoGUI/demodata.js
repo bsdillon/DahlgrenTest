c53536924648fa8f7c629f8ac4c26b0569a3b036
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-05-26 15:49:50 -0400

    Consolidate data into 1 file

--- RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
@@ -1,39 +1,30 @@
-let testCases = [
+var testCases = {
+testCase : [
   { Test: "Open Site", Status: "Pass", Message: "" },
   { Test: "Radio Test", Status: "Pass", Message: "" },
-  { Test: "Textbox Test", Status: "Pass", Message: "" },
+  { Test: "Textbox Test", Status: "Fail", Message: "" },
   { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
   { Test: "Input Test", Status: "Fail", Message:" "}
-];
-function loadData(){
-          var config1 = createConfig("Pass vs. Fail");
-          var chart1 = createChart("canvas", 350,350,config1);
-          let labels =['Fail','Pass', 'Not Run'];
-          setLabels(config1, labels);
-          var pass = 0;
-          var fail = 0;
-          var norun = 0;
-          for(let element of testCases){
-           if(element.Status == 'Pass') {
-            pass = pass + 1;
-           } else if(element.Status == 'Fail'){
-            fail = fail + 1;
-           }else{
-            norun = norun + 1;
-           }
-          }
-          data1 = [fail, pass, norun];
-          console.log(data1);
-          createDataSet(config1, data1);
-          chart1.update();
-          var suiteResult ='';
-          if(pass > fail){
-             suiteResult = 'Pass';
-          }else{
-             suiteResult = 'Fail';
-          }
-          console.log(suiteResult);
-}
+],
+
+testCase1 : [
+  { Test: "Open Site", Status: "Pass", Message: "" },
+  { Test: "Radio Test", Status: "Fail", Message: "Refused to run" },
+  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
+  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
+  { Test: "Input Test", Status: "Fail", Message:" "}
+],
+
+
+testCase2 : [
+  { Test: "Open Site", Status: "Pass", Message: "" },
+  { Test: "Radio Test", Status: "Pass", Message: "Refused to run" },
+  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
+  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
+  { Test: "Input Test", Status: "Not Run", Message:" "}
+],
+};
+
 function generateHeatMapHead(table, data) {
  let thead = table.createTHead();
  let row = thead.insertRow();
