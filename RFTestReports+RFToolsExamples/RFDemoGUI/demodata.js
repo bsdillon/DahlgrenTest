275b4bf2d5c2715ea18e0b50d532773f1497e82d
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-07 06:29:36 -0400

    Update demodata.js

--- RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
@@ -12,69 +12,23 @@ testRun1 : [
   { Test: "Radio Test", Status: "Fail", Message: "Refused to run" },
   { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
   { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Input Test", Status: "RunningMC", Message:" "}
+  { Test: "Input Test", Status: "Fail", Message:" "}
 ],
 
 
 testRun2 : [
-  { Test: "Open Site", Status: "Not Run", Message: "" },
-  { Test: "Radio Test", Status: "Not Run", Message: "" },
-  { Test: "Textbox Test", Status: "Not Run", Message: "Just didn't work" },
-  { Test: "Date Test", Status: "Not Run", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Input Test", Status: "Not Run", Message:"Running"}
+  { Test: "Open Site", Status: "Pass", Message: "" },
+  { Test: "Radio Test", Status: "Fail", Message: "" },
+  { Test: "Textbox Test", Status: "Pass", Message: "Just didn't work" },
+  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
+  { Test: "Input Test", Status: "Pass", Message:"Running"}
 ],
 
 testRun3 : [
-  { Test: "Open Site", Status: "Not Run", Message: "" },
-  { Test: "Radio Test", Status: "Not Run", Message: " " },
+  { Test: "Open Site", Status: "Pass", Message: "" },
+  { Test: "Radio Test", Status: "Running", Message: " " },
   { Test: "Textbox Test", Status: "Not Run", Message: "" },
   { Test: "Date Test", Status: "Not Run", Message: " " },
   { Test: "Input Test", Status: "Not Run", Message:" "}
 ],
 };
-
-function generateHeatMapHead(table, data) {
- let thead = table.createTHead();
- let row = thead.insertRow();
-}
-function generateHeatMap(table, data){
-  for(let element of data) {
-   let row = table.insertRow();
-   let cell = row.insertCell()
-   let text = document.createTextNode("");
-   cell.appendChild(text);
-   cell.style.height = "50";
-   cell.style.width ="50";
-   cell.style.backgroundColor = "blue";
-   console.log(element);
-   status = element.Status;
-   console.log(status);
-   if(status == 'Pass') {
-    cell.style.backgroundColor = "green";
-   }else if(status == 'Fail') {
-    cell.style.backgroundColor = "red";
-   }
- }
-}
-
-function generateTableHead(table, data) {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key of data) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
-}
-
-function generateTable(table, data) {
-  for (let element of data) {
-    let row = table.insertRow();
-    for (key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      cell.appendChild(text);
-    }
-  }
-}
