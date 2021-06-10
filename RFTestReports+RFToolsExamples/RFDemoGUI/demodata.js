2a822cc85baf68f323b55b08eb1a25c2e69df233
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-10 16:10:21 -0400

    Update demodata.js

--- RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
@@ -4,7 +4,7 @@ testRun0 : [
   { Test: "Radio Test", Status: "Pass", Message: "" },
   { Test: "Textbox Test", Status: "Pass", Message: "" },
   { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Input Test", Status: "Pass", Message:" "}
+  { Test: "Input Test", Status: "Fail", Message:" "}
 ],
 
 testRun1 : [
@@ -12,23 +12,38 @@ testRun1 : [
   { Test: "Radio Test", Status: "Fail", Message: "Refused to run" },
   { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
   { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Input Test", Status: "Fail", Message:" "}
+  { Test: "Input Test", Status: "Fail", Message:" "}, 
+  { Test: "Slider Test", Status: "Pass", Message:" "}
+
 ],
 
 
 testRun2 : [
   { Test: "Open Site", Status: "Pass", Message: "" },
-  { Test: "Radio Test", Status: "Fail", Message: "" },
-  { Test: "Textbox Test", Status: "Pass", Message: "Just didn't work" },
-  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Input Test", Status: "Pass", Message:"Running"}
+  { Test: "Radio Test", Status: "Pass", Message: "Refused to run" },
+  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
+  { Test: "Date Test", Status: "Pass", Message: "" },
+  { Test: "Input Test", Status: "Pass", Message:" "},
+  { Test: "Slider Test", Status: "Pass", Message:" "},
+  { Test: "Checkbox Test", Status: "Fail", Message:"No Element Found "}
 ],
 
 testRun3 : [
   { Test: "Open Site", Status: "Pass", Message: "" },
-  { Test: "Radio Test", Status: "Running", Message: " " },
-  { Test: "Textbox Test", Status: "Not Run", Message: "" },
-  { Test: "Date Test", Status: "Not Run", Message: " " },
-  { Test: "Input Test", Status: "Not Run", Message:" "}
+  { Test: "Radio Test", Status: "Pass", Message: "Refused to run" },
+  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
+  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
+  { Test: "Slider Test", Status: "Pass", Message:" "},
+  { Test: "Checkbox Test", Status: "Fail", Message:"No Element Found "}
+],
+
+testRun4 : [
+  { Test: "Open Site", Status: "Pass", Message: "" },
+  { Test: "Radio Test", Status: "Pass", Message: "Refused to run" },
+  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
+  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
+  { Test: "Slider Test", Status: "Running", Message:" "},
+  { Test: "Checkbox Test", Status: "Running", Message:"No Element Found "}
 ],
 };
+
