541d397f1b423ddbc674f357dd4c66a234430a05
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-14 12:02:06 -0400

    Update demoGUI.js

--- RFTestReports+RFToolsExamples/RFDemoGUI/demoGUI.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demoGUI.js
@@ -1,5 +1,5 @@
 //Home Page
- 
+
  function generateHeatMap(data, length){
     var heatMap = document.getElementById('heatmap');
     for(var j=0; j<length+1; j++){
@@ -9,7 +9,7 @@
      labelCell.appendChild(label);
      labelCell.style.color = "white";
      labelCell.style.backgroundColor = "Gray";
-     var array = data["testRun"+j]; 
+     var array = data["testRun"+j];
      if(!array){
       var image = document.createElement('img');
       image.src = "clockimage.png";
@@ -42,9 +42,9 @@
      if(!data["testRun" + (j-1)]){
        targetNum = targetNum;
      }else{
-       prevNum = Object.keys(data["testRun" + (j-1)]).length;  
+       prevNum = Object.keys(data["testRun" + (j-1)]).length;
        if(targetNum < prevNum){
-         targetNum = prevNum; 
+         targetNum = prevNum;
        }
       }
      for(var i=0; i<heatMap.rows.length; i++){
@@ -54,16 +54,16 @@
        }
      }
    }
-   var rowspan = heatMap.rows.length; 
+   var rowspan = heatMap.rows.length;
    var headerRuns = heatMap.rows[0].insertCell(heatMap.rows[0].cells[0]);
    headerRuns.rowSpan = rowspan;
    headerRuns.style.backgroundColor = "DimGray";
    headerRuns.style.color = "white";
-   var header = document.createTextNode("Test Runs"); 
-   headerRuns.appendChild(header);  
+   var header = document.createTextNode("Test Runs");
+   headerRuns.appendChild(header);
 }
 
-function toolTip(parent, text){
+ function toolTip(parent, text){
    parent.setAttribute('class','tooltip');
    var popUp = document.createElement("SPAN");
    popUp.setAttribute('class','tooltiptext');
@@ -71,7 +71,6 @@ function toolTip(parent, text){
    popUp.appendChild(popText);
    parent.appendChild(popUp);
 }
-
 
  function totalStatus(data){
    var stat = '';
@@ -84,15 +83,13 @@ function toolTip(parent, text){
    return stat;
  }
 
-
-function createButton(name, func, inner, element, parent, align){
-   var name = document.createElement("BUTTON");
+ function createButton(name, func, inner, element, window, parent, align){
+   var name = window.document.createElement("BUTTON");
    name.onclick = function(element){ return function(){ func(element);}}(element);
    name.innerHTML = inner;
    name.style.cssFloat = align;
    parent.appendChild(name);
  }
-
 
  function generateStatus(data, length){
         var div =document.createElement('div');
@@ -120,12 +117,11 @@ function createButton(name, func, inner, element, parent, align){
           }
           para.appendChild(span);
           buttonLink = 'testRun' + i;
-          createButton('button', oqe,'OQE', buttonLink, para, '');
+          createButton('button', oqe,'OQE', buttonLink, window, para, '');
      }
  }
 
- 
-function timedUpdate(tableid) {
+ function timedUpdate(tableid) {
          document.getElementById("dataSourceFile").remove();
          var table = document.getElementById(tableid);
          table.innerHTML = "";
@@ -146,7 +142,7 @@ function timedUpdate(tableid) {
         setTimeout(function(){ timedUpdate('heatmap'); }, 5000);
  }
 
-function sortResults(data){
+ function sortResults(data){
    var pass = 0;
    var fail = 0;
    for(let element of data){
@@ -158,43 +154,187 @@ function sortResults(data){
    }
     data1 = [fail, pass];
     return data1;
-}
+ }
 
  function oqe(x){
      var link = x +".html";
      var windowFeatures = "menubar=yes,location=yes,resizable=no,scrollbars=yes,status=yes,left=100,top=100";
      var newWindow = null;
-     if(x == 'total'){
-        var winTotal = window.open(link, newWindow, windowFeatures);
-        if( win != null){win.close();}
+     if(x != 'total'){
+       var chartData = testRuns[x];
+       var win = window.open('', newWindow, windowFeatures);
+       win.addEventListener('load', loadOQEChart(chartData, win), false);
      }else{
-        var chartData = testRuns[x];
-        var win = window.open('', newWindow, windowFeatures);
-        win.addEventListener('load', loadOQEChart(chartData, win), false);
-
+       var win = window.open('', newWindow, windowFeatures);
+       win.addEventListener('load', startData(win), false );
      }
  }
+
+//Cumulative OQE Metrics
+ function startData(win){
+      var initSubClass = [];
+      for(var i=0; i<Object.keys(testRuns).length; i++){
+         var initClass = "hidden";
+         initSubClass.push(initClass);
+      }
+      loadTable(testRuns, initSubClass, win);
+      loadTotalData(testRuns, win);
+    }
+
+ function loadTable(object, subclass, win){
+     var j = 0;
+     win.document.body.innerHTML = '';
+     win.document.title = 'Total OQE Results';
+     createHeaders('totalHeader', 'Total OQE Results', '1', win);
+     var table = initTable(win);
+     var data = Object.keys(object);
+     let thead = table.createTHead();
+     let row = thead.insertRow();
+     let title = win.document.createTextNode("Test Runs");
+     let cell = row.insertCell();
+     cell.appendChild(title);
+     for(let element of data){
+      let row = table.insertRow();
+      let cell = row.insertCell();
+      let text = win.document.createTextNode("Test Run " + j + "\u00A0");
+      cell.appendChild(text);
+      var expand = win.document.createElement("BUTTON");
+      expand.onclick = function(element, win){ return function(){ expandTable(element, win);}}(element, win);
+      expand.innerHTML = ' + ';
+      expand.style.cssFloat = "right";
+      cell.appendChild(expand);
+      createButton('oqeLink', oqe, 'OQE', element, win, cell, "right");
+      var actualdata = object[element];
+      var data1 = Object.keys(actualdata[0]);
+      var subtable = win.document.createElement("TABLE");
+      win.document.body.appendChild(subtable);
+      subtable.id = "sub" + element;
+      createStyle('hidden', win, 'display: none;');
+      createStyle('show', win, 'border: 1px solid black;');
+      subtable.className = subclass[j];
+      generateTableHead(subtable, data1, win);
+      generateTable(subtable, actualdata, win);
+      cell.appendChild(subtable);
+      j = j + 1;
+     }
+    }
+
+ function initTable(win){
+  var table = win.document.createElement("TABLE");
+  table.setAttribute('border', '5px solid black');
+  win.document.body.appendChild(table);
+  return table;
+ }
+
+ function expandTable(element, win) {
+      var hiddenTable = win.document.getElementById("sub" + element);
+      if(hiddenTable.className == 'hidden'){
+       hiddenTable.setAttribute('class', 'show');
+      }else if(hiddenTable.className == 'show'){
+       hiddenTable.setAttribute('class', 'hidden');
+      }
+    }
+
+ function createStyle(styleName, win, spec){
+   var style = win.document.createElement('style');
+   style.type = 'text/css';
+   styleString = '.'+styleName + ' { ' +spec +' }';
+   style.innerHTML = styleString;
+   win.document.getElementsByTagName('head')[0].appendChild(style);
+ }
+
+ function sortData(object, breakElement){
+    var passPercent =[];
+    var failPercent =[];
+    var failA = [];
+    var passA = [];
+    var otherA = [];
+    var data = Object.keys(object);
+    for(let element of data){
+       var subdata = testRuns[element];
+       var length = Object.keys(subdata).length;
+       data1 = sortResults(subdata);
+       passA.push(data1[1]);
+       failA.push(data1[0]);
+       otherA.push(data1[2]);
+       var passP = (data1[1] / (data1[1] + data1[0]))*100;
+       var failP = (data1[0] /(data1[1] + data1[0]))*100;
+       passPercent.push(passP);
+       failPercent.push(failP);
+       if(element == breakElement){break;}
+    }
+    var returnArray = [passPercent, failPercent, failA, passA, otherA];
+    return returnArray;
+  }
+
+ function sumArray(a, total){
+       var total=0;
+       for(var i in a) {
+         total += a[i];
+       }
+       return total;
+     }
+
+ function loadTotalData(object, win){
+      var length = Object.keys(object).length;
+      var breakElement = 'testRun' + length-1;
+      //Pie Chart
+      createHeaders('firstheader', 'Overall Test Breakdown', '2', win);
+      var config1 = createConfig("Pass vs. Fail");
+      var chart1 = createChart("canvas", 350,350,config1, win);
+      let labels =['Fail','Pass'];
+      setLabels(config1, labels);
+      var sortedData = sortData(object, breakElement);
+      data2 = [sumArray(sortedData[2]), sumArray(sortedData[3])];
+      createDataSet(config1, data2);
+      chart1.update();
+      //Line Chart
+      createHeaders('secondheader', "Performance Over Time", '2', win);
+      var config2 = createLineConfig(LINE_TYPE, "Pass/Fail Percentage vs. Test Run", "Test Run", "Percentage");
+      var chart2 = createLineChart("canvas2", 350, 350, config2, win);
+      chart2.id = 'chart2';
+      var XData = Object.keys(object);
+      setLineChartLabels(config2, XData);
+      createLineDataSet(config2, "Pass Percentage", sortedData[0], LINE_TYPE);
+      createLineDataSet(config2, "Fail Percentage", sortedData[1], LINE_TYPE);
+      chart2.update();
+      setTimeout(function(){ timedTotalUpdate('total', testRuns, win); }, 5000);
+     }
+
+ function timedTotalUpdate(tableid, object, win) {
+      var data = Object.keys(object);
+      var previousClass =[];
+      const isClosed = win.closed;
+      if(!isClosed){
+      for (let element of data){
+          var table = win.document.getElementById("sub" + element).className;
+          previousClass.push(table);
+       }
+       loadTable(testRuns, previousClass, win);
+       loadTotalData(testRuns, win);
+      }
+ }
+
+ function createHeaders(name, innerHTML, number, win){
+   var name = win.document.createElement("H"+number);
+   name.innerHTML = innerHTML;
+   win.document.body.appendChild(name);
+
+ }
+
 //Individual OQE Metrics
  function loadOQEChart(data, win){
    win.document.body.innerHTML = '';
    win.document.title = 'Individual OQE';
-   var header1 = win.document.createElement("H1");
-   header1.innerHTML = "Test Results";
-   win.document.body.appendChild(header1);
+   createHeaders('header1', 'Test Results', '1', win);
    var buttonParent = win.document.createElement("H2");
-   createButton('totalOQE', oqe, 'Total OQE', 'total', buttonParent, '');
+   createButton('totalOQE', oqe, 'Total OQE', 'total',win, buttonParent, '');
    win.document.body.appendChild(buttonParent);
    var dataKeys = Object.keys(data[0]);
-   var table = win.document.createElement('TABLE');
-   table.id = 'demo2';
-   table.setAttribute('border', '5px solid black');
-   console.log(table);
-   win.document.body.appendChild(table);
+   var table = initTable(win);
    generateTableHead(table, dataKeys, win);
    generateTable(table, data, win);
-   var header2 = win.document.createElement("H2");
-   header2.innerHTML = "Pass/Fail Graphic";
-   win.document.body.appendChild(header2);
+   createHeaders('header2', "Pass/Fail Graphic", '2', win);
    data1 = sortResults(data);
    var config1 = createConfig("Pass vs. Fail");
    var chart1 = createChart("canvas", 350,350,config1, win);
@@ -237,129 +377,3 @@ function sortResults(data){
    head.appendChild(script);
    loadOQEChart(data);
 }
-
-//Cumulative OQE Metrics
-
-
- function loadTable(object, subclass){
-     var j = 0;
-     var table = document.getElementById("total");
-     var data = Object.keys(object);
-     let thead = table.createTHead();
-     let row = thead.insertRow();
-     let title = document.createTextNode("Test Runs");
-     let cell = row.insertCell();
-     cell.appendChild(title);
-     for(let element of data){
-      let row = table.insertRow();
-      let cell = row.insertCell();
-      let text = document.createTextNode("Test Run " + j + "\u00A0");
-      cell.appendChild(text);
-      createButton('expand', expandTable, ' + ', element, cell, "right");
-      createButton('oqeLink', oqe, 'OQE', element, cell, "right");
-      var actualdata = object[element];
-      var data1 = Object.keys(actualdata[0]);
-      var subtable = document.createElement("TABLE");
-      subtable.id = "sub" + element;
-      subtable.setAttribute('class', subclass[j]);
-      generateTableHead(subtable, data1, window);
-      generateTable(subtable, actualdata, window);
-      cell.appendChild(subtable);
-      j = j + 1;
-     }
-    }
-
-
- function expandTable(element) {
-      var hiddenTable = document.getElementById("sub" + element);
-      if(hiddenTable.className == 'hidden'){
-       hiddenTable.setAttribute('class', 'show');
-      }else if(hiddenTable.className == 'show'){
-       hiddenTable.setAttribute('class', 'hidden');
-      }
-    }
-
- function sortData(object, breakElement){
-    var passPercent =[];
-    var failPercent =[];
-    var failA = [];
-    var passA = [];
-    var otherA = [];
-    var data = Object.keys(object);
-    for(let element of data){
-       var subdata = testRuns[element];
-       var length = Object.keys(subdata).length;
-       data1 = sortResults(subdata);
-       passA.push(data1[1]);
-       failA.push(data1[0]);
-       otherA.push(data1[2]);
-       var passP = (data1[1] / (data1[1] + data1[0]))*100;
-       var failP = (data1[0] /(data1[1] + data1[0]))*100;
-       passPercent.push(passP);
-       failPercent.push(failP);
-       if(element == breakElement){break;}
-    }
-    var returnArray = [passPercent, failPercent, failA, passA, otherA];
-    return returnArray;
-  }
-
- function sumArray(a, total){
-       var total=0;
-       for(var i in a) {
-         total += a[i];
-       }
-       return total;
-     }
-
- function loadTotalData(object){
-      var length = Object.keys(object).length;
-      var breakElement = 'testRun' + length-1;
-      var config2 = createLineConfig(LINE_TYPE, "Pass/Fail Percentage vs. Test Run", "Test Run", "Percentage");
-      var chart2 = createLineChart("canvas2", 350, 350, config2);
-      chart2.id = 'chart2';
-      var XData = Object.keys(object);
-      setLineChartLabels(config2, XData);
-      var config1 = createConfig("Pass vs. Fail");
-      var chart1 = createChart("canvas1", 350,350, config1, 'window');
-      chart1.id = 'chart1';
-      let labels =['Fail','Pass'];
-      setLabels(config1, labels);
-      var sortedData = sortData(object, breakElement);
-      data2 = [sumArray(sortedData[2]), sumArray(sortedData[3])];
-      createDataSet(config1, data2);
-      chart1.update();
-      createLineDataSet(config2, "Pass Percentage", sortedData[0], LINE_TYPE);
-      createLineDataSet(config2, "Fail Percentage", sortedData[1], LINE_TYPE);
-      chart2.update();
-      setTimeout(function(){ timedTotalUpdate('total', testRuns); }, 5000);
-     }
-
-
- function timedTotalUpdate(tableid, object) {
-      var data = Object.keys(object);
-      var previousClass =[];
-      for (let element of data){
-       var table = document.getElementById("sub" + element);
-       previousClass.push(table.className);
-      }
-      document.getElementById("dataSourceFile").remove();
-      var table = document.getElementById(tableid);
-      table.innerHTML = "";
-      var head= document.getElementsByTagName('head')[0];
-      var script= document.createElement('script');
-      script.src= 'demodata.js';
-      script.id='dataSourceFile';
-      head.appendChild(script);
-      loadTable(testRuns, previousClass);
-      loadTotalData(testRuns);
- }
-
- function startData(){
-      var initSubClass = [];
-      for(var i=0; i<Object.keys(testRuns).length; i++){
-         var initClass = "hidden";
-         initSubClass.push(initClass);
-      }
-      loadTable(testRuns, initSubClass);
-      loadTotalData(testRuns);
-    }
