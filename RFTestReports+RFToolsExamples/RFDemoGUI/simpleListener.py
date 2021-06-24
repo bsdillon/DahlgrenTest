4c323be031719f7a1cf04a9c1aef45dc2f1ec4de
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-24 11:07:59 -0400

    Add files via upload

--- RFTestReports+RFToolsExamples/RFDemoGUI/simpleListener.py
+++ RFTestReports+RFToolsExamples/RFDemoGUI/simpleListener.py
@@ -2,7 +2,7 @@ ROBOT_LISTENER_API_VERSION = 2
 
 
 def start_suite(name, attrs):
-    testrun = attrs['longname']
+    testrun = attrs['id']
     with open("demodata.js", 'r') as file:
         lines = file.readlines()
         new_last_line = " "
@@ -19,7 +19,7 @@ def end_test(name, attrs):
     message = attrs['message']
     timepassed = attrs['elapsedtime']
     with open("demodata.js", 'a') as file:
-        file.write("{ Test: " + testname +", Status: " + status +", Message: " + message +"}, \n")
+        file.write("{ Test: '" + testname +"', Status: '" + status +"', Message: '" + message +"'}, \n")
 
 def end_suite(name, attrs):
     with open("demodata.js", 'a') as file:
