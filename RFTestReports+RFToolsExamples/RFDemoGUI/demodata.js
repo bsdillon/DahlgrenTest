b1aa7c53b8da930844e9d47a47f9663cadd31463
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-24 10:54:37 -0400

    Add files via upload

--- RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
@@ -1,27 +1,27 @@
 var testRuns = {
 
- Testcode Withtags20210624 10:10:23.642 : [
+ Testcode Withtags : [
 { Test:  Open Site, Status: PASS, Message: }, 
 { Test:  Radio Test, Status: PASS, Message: }, 
 { Test: Textbox Test, Status: PASS, Message: }, 
 { Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
 ], 
- Testcode Withtags20210624 10:11:49.119 : [
+ Testcode Withtags : [
 { Test:  Open Site, Status: PASS, Message: }, 
 { Test:  Radio Test, Status: PASS, Message: }, 
 { Test: Textbox Test, Status: PASS, Message: }, 
 { Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
 ], 
- Testcode Withtags20210624 10:21:03.223 : [
+ Testcode Withtags : [
 { Test:  Open Site, Status: PASS, Message: }, 
 { Test:  Radio Test, Status: PASS, Message: }, 
 { Test: Textbox Test, Status: PASS, Message: }, 
 { Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
 ], 
- Testcode Withtags20210624 10:22:24.631 : [
+ Testcode Withtags : [
 { Test:  Open Site, Status: PASS, Message: }, 
 { Test:  Radio Test, Status: PASS, Message: }, 
 { Test: Textbox Test, Status: PASS, Message: }, 
 { Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
 ], 
- }
+ };
