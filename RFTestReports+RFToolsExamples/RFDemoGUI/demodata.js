f8560a8099bba4277f45d87cab153d0aa3ffd91d
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-24 11:00:27 -0400

    Add files via upload

--- RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
@@ -1,24 +1,18 @@
 var testRuns = {
 
- Testcode Withtags : [
+ s1 : [
 { Test:  Open Site, Status: PASS, Message: }, 
 { Test:  Radio Test, Status: PASS, Message: }, 
 { Test: Textbox Test, Status: PASS, Message: }, 
 { Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
 ], 
- Testcode Withtags : [
+ s1 : [
 { Test:  Open Site, Status: PASS, Message: }, 
 { Test:  Radio Test, Status: PASS, Message: }, 
 { Test: Textbox Test, Status: PASS, Message: }, 
 { Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
 ], 
- Testcode Withtags : [
-{ Test:  Open Site, Status: PASS, Message: }, 
-{ Test:  Radio Test, Status: PASS, Message: }, 
-{ Test: Textbox Test, Status: PASS, Message: }, 
-{ Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
-], 
- Testcode Withtags : [
+ s1 : [
 { Test:  Open Site, Status: PASS, Message: }, 
 { Test:  Radio Test, Status: PASS, Message: }, 
 { Test: Textbox Test, Status: PASS, Message: }, 
