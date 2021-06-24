232a16df306487bd19ec028d7f74f9b6094a357d
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-24 10:27:34 -0400

    Add files via upload

--- RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
@@ -1,42 +1,27 @@
 var testRuns = {
-testRun0 : [
-  { Test: "Open Site", Status: "Pass", Message: "" },
-  { Test: "Radio Test", Status: "Pass", Message: "" },
-  { Test: "Textbox Test", Status: "Pass", Message: "" },
-  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Input Test", Status: "Fail", Message:" "}
-],
 
-testRun1 : [
-  { Test: "Open Site", Status: "Pass", Message: "" },
-  { Test: "Radio Test", Status: "Fail", Message: "Refused to run" },
-  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
-  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Input Test", Status: "Fail", Message:" "}, 
-  { Test: "Slider Test", Status: "Pass", Message:" "}
-
-],
-
-
-testRun2 : [
-  { Test: "Open Site", Status: "Pass", Message: "" },
-  { Test: "Radio Test", Status: "Pass", Message: "Refused to run" },
-  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
-  { Test: "Date Test", Status: "Pass", Message: "" },
-  { Test: "Input Test", Status: "Pass", Message:" "},
-  { Test: "Slider Test", Status: "Pass", Message:" "},
-  { Test: "Checkbox Test", Status: "Fail", Message:"No Element Found "}
-],
-
-testRun3 : [
-  { Test: "Open Site", Status: "Pass", Message: "" },
-  { Test: "Radio Test", Status: "Pass", Message: "Refused to run" },
-  { Test: "Textbox Test", Status: "Fail", Message: "Just didn't work" },
-  { Test: "Date Test", Status: "Fail", Message: "Element 'date' should have contained text '13/12/2020' but its text was ' '." },
-  { Test: "Slider Test", Status: "Pass", Message:" "},
-  { Test: "Checkbox Test", Status: "Fail", Message:"No Element Found "}
+ Testcode Withtags20210624 10:10:23.642 : [
+{ Test:  Open Site, Status: PASS, Message: }, 
+{ Test:  Radio Test, Status: PASS, Message: }, 
+{ Test: Textbox Test, Status: PASS, Message: }, 
+{ Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
 ], 
-
-};
-
-
+ Testcode Withtags20210624 10:11:49.119 : [
+{ Test:  Open Site, Status: PASS, Message: }, 
+{ Test:  Radio Test, Status: PASS, Message: }, 
+{ Test: Textbox Test, Status: PASS, Message: }, 
+{ Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
+], 
+ Testcode Withtags20210624 10:21:03.223 : [
+{ Test:  Open Site, Status: PASS, Message: }, 
+{ Test:  Radio Test, Status: PASS, Message: }, 
+{ Test: Textbox Test, Status: PASS, Message: }, 
+{ Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
+], 
+ Testcode Withtags20210624 10:22:24.631 : [
+{ Test:  Open Site, Status: PASS, Message: }, 
+{ Test:  Radio Test, Status: PASS, Message: }, 
+{ Test: Textbox Test, Status: PASS, Message: }, 
+{ Test:  Date Test, Status: FAIL, Message: Element 'date' should have contained text '12/12/2020' but its text was ''.}, 
+], 
+ }
