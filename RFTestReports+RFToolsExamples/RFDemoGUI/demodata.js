b2789b1764389029c13e10460062af6d1bddcc5b
laurenemreyn <66188217+laurenemreyn@users.noreply.github.com>
2021-06-24 11:51:31 -0400

    Add files via upload

--- RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
+++ RFTestReports+RFToolsExamples/RFDemoGUI/demodata.js
@@ -1,23 +1,23 @@
 var testRuns = {
- 20210624 11:40:19.233 : [
+ "20210624 11:45:41.189" : [
 { Test: ' Open Site', Status: 'PASS', Message: ""}, 
 { Test: ' Radio Test', Status: 'PASS', Message: ""}, 
 { Test: 'Textbox Test', Status: 'PASS', Message: ""}, 
 { Test: ' Date Test', Status: 'FAIL', Message: "Element 'date' should have contained text '12/12/2020' but its text was ''."}, 
 ], 
- 20210624 11:40:57.317 : [
+ "20210624 11:46:43.143" : [
 { Test: ' Open Site', Status: 'PASS', Message: ""}, 
 { Test: ' Radio Test', Status: 'PASS', Message: ""}, 
 { Test: 'Textbox Test', Status: 'PASS', Message: ""}, 
 { Test: ' Date Test', Status: 'FAIL', Message: "Element 'date' should have contained text '12/12/2020' but its text was ''."}, 
 ], 
- 20210624 11:41:23.628 : [
+ "20210624 11:47:41.497" : [
 { Test: ' Open Site', Status: 'PASS', Message: ""}, 
 { Test: ' Radio Test', Status: 'PASS', Message: ""}, 
 { Test: 'Textbox Test', Status: 'PASS', Message: ""}, 
 { Test: ' Date Test', Status: 'FAIL', Message: "Element 'date' should have contained text '12/12/2020' but its text was ''."}, 
 ], 
- 20210624 11:42:04.632 : [
+ "20210624 11:48:43.847" : [
 { Test: ' Open Site', Status: 'PASS', Message: ""}, 
 { Test: ' Radio Test', Status: 'PASS', Message: ""}, 
 { Test: 'Textbox Test', Status: 'PASS', Message: ""}, 
