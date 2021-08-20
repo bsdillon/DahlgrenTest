efc3d2d6ca5f61519289228f63ce014879a355e8
MaxwellMasaitis <maxwelljmasaitis@gmail.com>
2021-08-20 15:57:14 -0400

    Some Java test files for comparison to the C++ files

---
+++ SQM/test_java01/JAVACode/JAVAArrayAccessCheck/ArrayAccessCheck.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Point{
+	
+	private int x = 0, y = 0;
+	
+	public Point(int a, int b){
+		x = a;
+		y = b;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	public void setXY(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class ArrayAccessCheck{
+
+     public static void main(String[] args){
+		Point newPoint = new Point(10, 24);
+        Point[] pointArray = new Point[] {new Point(1,2), new Point(3,4), new Point(45,56), newPoint};
+        int pos = 2;
+        int output = pointArray[pos].getX();
+		pointArray[0].setY(20);
+		pointArray[1].setXY(4, 20);
+        output = pointArray[3].getY();
+        System.out.println(output);
+     }
+}
