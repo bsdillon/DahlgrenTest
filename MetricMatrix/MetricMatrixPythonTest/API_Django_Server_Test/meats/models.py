15145c35d9b4a334dde914e9219062d3d1486000
MaxwellMasaitis <maxwelljmasaitis@gmail.com>
2021-10-22 12:55:38 -0400

    Compressed Metric Matrix test files

--- MetricMatrix/MetricMatrixPythonTest/API_Django_Server_Test/meats/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Meat(models.Model):
-    color = models.CharField(max_length = 100)
-    weight = models.CharField(max_length = 100)
-    flavor = models.CharField(max_length = 100)
-    personality = models.CharField(max_length = 100)
--- MetricMatrix/MetricMatrixPythonTest/API_Django_Server_Test/meats/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Meat
-
-class MeatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meat
-        fields = ["id", "color", "weight", "flavor", "personality"]
