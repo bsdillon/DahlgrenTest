82486fb2418119315f51a7f48d11660640605e06
MaxwellMasaitis <maxwelljmasaitis@gmail.com>
2021-11-15 17:32:32 -0500

    New permission

--- MetricMatrix/holisticPrototypeAPI/Lab101ClientApp.py
+++ MetricMatrix/holisticPrototypeAPI/Lab101ClientApp.py
@@ -2,14 +2,17 @@ import requests
 import json
 import base64
 
-debugUser = "admin"#"lab101"
+debugUser = "admin"#"bogus"#"lab101"
 if (debugUser == "admin"):
     client_id = "uxsjRcQXcF8kcjrCAVRnUJNbgUrsOmkNe2BADNtd"
     secret = "0KfnA11Y5sPwNdK3733PILl1VMDbromsVINNrWXhQtZPuKKxPJYaoekHtgVxjtkKS8on9IS9JbWNDJZclOuE3CDBOd8pvqaxgZq8YJcoTVOx0Mt7zawZ0ecraKMEdI4u"
 elif (debugUser == "lab101"):
     client_id = "IfohbSKEwENGyQWBNl15qpI81lt1phiOplvhYmV6"
     secret = "9QzIgOuZoXwYzPoS57VIngjbLrPAU1WRu9FXqJJCLNfHrUTUPKKIFbj82XIph6LYfZw780XL0tu9YXVEdSamOY8imfNXrJvZRlM0nsAlZueNKfNuGansx9iOh22CmwAw"
-
+elif (debugUser == "bogus"):
+    client_id = "3NlXru6PPbpouGGPbmo2kQr1H5Xu8gROt1bFjwos"
+    secret = "yOyR9Z1pkt1Di0Zw6wmYUl7jVKNivvg89JY2FaW5JxJEqD7NlrtVgjK8PN6FSUyIf890M3yY4TUCdBAyzNkIFIsUTM8pZCNwyD0cuIOW8xXAzF36M0KUVVazei6nKRWW"
+    
 cert = "../venv/Lib/site-packages/sslserver/certs/development.crt"
 
 # post for a token using Client Credentials
@@ -25,6 +28,7 @@ def getAccessToken(client_id, secret, cert):
     
     tokenResponse = requests.post(token_url, headers=headers, data={"grant_type": "client_credentials"}, verify = cert)
     #print(json.dumps(tokenResponse.json(), indent = 2))
+    # should maybe push out the error response if there is one e.g. with "bogus"
     return tokenResponse.json()["access_token"]
 
 access_token = getAccessToken(client_id, secret, cert)
@@ -34,6 +38,7 @@ print(json.dumps(response.json(), indent = 2))
 
 # try another request
 #access_token = getAccessToken(client_id, secret, cert)
-#newLab = {"name": "lab102", "UIC": 102}
+#newLab = {"name": "lab104", "UIC": 104}
 #response = requests.post("https://localhost:8000/labs/", json = newLab, headers={"Authorization": "Bearer "+access_token}, verify = cert)
+#response = requests.delete("https://localhost:8000/labs/lab104", headers={"Authorization": "Bearer "+access_token}, verify = cert)
 #print(json.dumps(response.json(), indent = 2))
--- MetricMatrix/holisticPrototypeAPI/holisticPrototype/views.py
+++ MetricMatrix/holisticPrototypeAPI/holisticPrototype/views.py
@@ -5,16 +5,29 @@ from .models import Lab, Test
 from .serializers import LabSerializer, TestSerializer
 
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
+# Custom permissons for client credentials
+
+class ClientCredentialPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.auth is None:
+            return False
+        grant_type = request.auth.application.get_authorization_grant_type_display()
+        if request.user is None and grant_type == "Client credentials":
+            request.user = request.auth.application.user # The application user defined in the admin screen! Currently only "admin" has a user - it's admin
+            return True
+        else:
+            return False
+
 # Create your views here.
 
 class LabViewSet(viewsets.ModelViewSet):
-    permission_classes = [TokenHasReadWriteScope]
+    permission_classes = [ClientCredentialPermission, TokenHasReadWriteScope]#, permissions.IsAdminUser]
     serializer_class = LabSerializer
     queryset = Lab.objects.all()
     lookup_field = "name"
 
 class TestViewSet(viewsets.ModelViewSet):
-    permission_classes = [TokenHasReadWriteScope]
+    permission_classes = [ClientCredentialPermission, TokenHasReadWriteScope]#, permissions.IsAdminUser]
     serializer_class = TestSerializer
     #queryset = Test.objects.all()
     def get_queryset(self):
