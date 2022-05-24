d9d3842b44df9d6d85d7168e2c7eadee905c5881
parkinsonlee <85889426+parkinsonlee@users.noreply.github.com>
2022-05-24 15:08:06 -0400

    Added support for static functions and changed how return types are saved

--- MBSE_Library/Lib/plantumlclass.py
+++ MBSE_Library/Lib/plantumlclass.py
@@ -1,4 +1,3 @@
-
 class Plantumlclass:
     classlist = []
     def __init__(self, UMLclass):
@@ -18,29 +17,20 @@ class Plantumlclass:
     def getClassName(self):
         return self.UMLclass
 
-    def addvoidmethod(self, method):
-        m1 = Plantumlmethod(method, "void")
-        self.methods.append(m1)
-
-    def addmethodint(self, method):
-        m1 = Plantumlmethod(method, "Int")
-        self.methods.append(m1)
-
-    def addmethodstring(self, method):
-        m1 = Plantumlmethod(method, "String")
-        self.methods.append(m1)
-
     def addmethod(self, method):
         m1 = Plantumlmethod(method, "")
         self.methods.append(m1)
+        return m1
 
     def printUML(self):
         #print(self.UMLclass)
         #print(self.methods)
 
         file1 = open(self.UMLclass + ".txt", "w")
-        content = [self.UMLclass + ' {\n' + ' \n']
+        content = ["class " + self.UMLclass + ' {\n' + ' \n--\n']
         for i in self.methods:
+            if i.getStatic():
+                content += '--'
             content += i.getMethod()
             content += ': '
             content += i.getType()
@@ -49,6 +39,7 @@ class Plantumlclass:
         self.content = content
         file1.writelines(content)
         file1.close()
+        # need to pull image from plant uml
 
     def getcontent(self):
         return self.content
@@ -57,9 +48,19 @@ class Plantumlmethod:
     def __init__(self, method, type):
         self.method = method
         self.type = type
+        self.static = False
 
     def getMethod(self):
         return self.method
 
     def getType(self):
         return self.type
+
+    def setType(self, type):
+        self.type = type
+
+    def setStatic(self):
+        self.static = True
+
+    def getStatic(self):
+        return self.static
--- MBSE_Library/main.py
+++ MBSE_Library/main.py
@@ -1,9 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -12,8 +6,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 # Import Multiplication from your library
 from Lib import Multiplication
@@ -25,16 +17,77 @@ multiplication = Multiplication(2)
 print(multiplication.multiply(5))
 
 from Lib import Plantumlclass
+
 c1 = Plantumlclass("class1")
 
-c1.addvoidmethod("c1m1")
-c1.addmethodstring("c1m2")
+c1.addmethod("c1m1")
+c1.addmethod("c1m2")
 
 c2 = Plantumlclass("class2")
-c2.addmethodint("c2m1")
-c1.addmethod("c1m3")
+c2.addmethod("c2m1")
+
+m1 = c1.addmethod("c1m3")
+m1.setStatic()
+m1.setType("String")
+
 c1.printUML()
 c2.printUML()
 
-#need to pull image from plant uml
+numinput = 0
+
+while numinput != 4:
+    print("1 - Add a class")
+    print("2 - Add a method")
+    print("3 - Print diagram")
+    print("4 - Exit")
+
+    numinput = int(input())
+
+    if numinput == 1:
+        print("What is the name of the class?")
+        classname = input()
+        Plantumlclass(classname)
+
+    elif numinput == 2:
+        classlist = Plantumlclass.getClasslist()
+        print("Which class would you like to add a method to?")
+        j = 1
+        for i in classlist:
+            print(j, str(":"), i.getClassName())
+            j = j + 1
+
+        print(j, ": Cancel")
+        methodinput = int(input())
+        if methodinput < j:
+            methodinput = methodinput - 1
+            c = classlist[methodinput]
+            print("What is the name of the method?")
+            methodname = input()
+            m = c.addmethod(methodname)
+            print("Static Y/N?")
+            answer = input()
+            if answer == str("Y") or answer == str("y"):
+                m.setStatic()
+            print("What will the return type be?")
+            print("1 - Void")
+            print("2 - Int")
+            print("3 - String")
+            print("4 - ?")
+            returntype = int(input())
+            if returntype == 1:
+                m.setType("void")
+            if returntype == 2:
+                m.setType("int")
+            if returntype == 3:
+                m.setType("String")
+            if returntype == 4:
+                m.setType("")
+
+    elif numinput == 3:
+        classlist = Plantumlclass.getClasslist()
+        for i in classlist:
+            i.printUML()
+    else:
+        break
+
 #containorization
