72a7ae5719cb5bc250b6fa81a833b3c4a6933c90
Austin James Fitzhugh afitz010 <austin.j.fitzhugh@gmail.com>
2022-07-08 10:10:30 -0400

    Added relevant comments to parse through affectedline/tree

---
+++ SQM/SoftwareAnalyzer2/Structure/Metrics/PlantUMLAPI.cs
@@ -0,0 +1,62 @@
+using SoftwareAnalyzer2.Structure.Metrics;
+using SoftwareAnalyzer2.Structure.Gephi;
+using SoftwareAnalyzer2.Structure.Graphing;
+using SoftwareAnalyzer2.Structure.Node;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareAnalyzer2.Structure.Metrics
+{
+    class PlantUMLAPI
+    {
+//        private static string PlantUMLReport = "";
+        // TODO: Needs to return an int if used as reportCount in ModuleNavigator.cs
+       internal static void CreatePlantUMLAPI(string filename)
+       {
+//            StreamWriter writer = new StreamWriter(filename);
+//            StringBuilder sb = new StringBuilder();
+//            // Create server (run Python server file)
+//            string cmdText = "python server.py";
+//            System.Diagnostics.Process.Start("powershell.exe", cmdText);
+//            PopulateData();
+//
+//            writer.WriteLine(PlantUMLReport);
+//            writer.Close();
+//        }
+//        internal static void PopulateData()
+//        {
+//            PlantUMLReport += "@startuml \r\n";
+//            foreach (AbbreviatedGraph type in MetricUtilities.AllMembers)
+//            {
+//                if (type.Represented.Node.Equals(Members.INTERFACE) || type.Represented.FileName.Equals("--"))
+//                {
+//                    //skip any interface or external nodes. We don't classify them as the program interface
+//                    continue;
+//                }
+//                if (type.Represented.Node.Equals(Members.CLASS))
+//                {
+//                    System.Diagnostics.Process.Start("http://localhost:8080/create_class/" + type.Represented.Code);
+//                }
+//                AbbreviatedGraph[] members = MetricUtilities.GetMembersOf(type);
+//                if (members.Length == 0)
+//                {
+//                    //this class has no members
+//                    continue;
+//                }
+//                foreach (AbbreviatedGraph member in members)
+//                {
+//                    if (member.Represented.Node.Equals(Members.Method))
+//                    {
+//                        PlantUMLReport += member.Represented.Node.ToString() + " " + member.Represented.Code + System.Environment.NewLine;
+//                   }
+//                }
+//                PlantUMLReport += "} \r\n";
+//            }
+//            PlantUMLReport += "@enduml";
+        }
+    }
+}
