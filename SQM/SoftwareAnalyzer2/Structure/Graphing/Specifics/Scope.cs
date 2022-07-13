eb0501b75cc63ced3e5d4d8cb4efb56483f34b3d
vKishchenko <vitaliy.kishchenko@gmail.com>
2022-07-13 11:29:54 -0400

    Skipping unassigned operator statements in linking CPP

--- SQM/SoftwareAnalyzer2/Structure/Graphing/Specifics/Scope.cs
+++ SQM/SoftwareAnalyzer2/Structure/Graphing/Specifics/Scope.cs
@@ -150,6 +150,10 @@ namespace SoftwareAnalyzer2.Structure.Graphing.Specifics
                 {
                     //no-op; could be linked with the break or continue when those appear.
                 }
+                else if (type.Equals(Members.Operator))
+                {
+                    //no op for operators which aren't assigned to any variables
+                }
                 else
                 {
                     //Very messy logically, but this will catch anything else which comes in a scope.
