a217ce0baa85d401257a3163fb051817dc795b7b
vKishchenko <vitaliy.kishchenko@gmail.com>
2022-07-29 17:40:12 -0400

    CPP if statement boolean expression updated

--- SQM/SoftwareAnalyzer2/Structure/Graphing/Specifics/Branch.cs
+++ SQM/SoftwareAnalyzer2/Structure/Graphing/Specifics/Branch.cs
@@ -39,20 +39,24 @@ namespace SoftwareAnalyzer2.Structure.Graphing.Specifics
         {
             base.Register(memberList, parentScope);
             INavigable myNavigable = (INavigable)represented;
-            AddControlExpression(myNavigable.GetNthChild(0));
+            AddControlExpression(myNavigable.GetFirstSingleLayer(Members.Boolean));
             //Add the scope for each branch and recurse
 
-            thenScope = RegisterScope(myNavigable.GetNthChild(1).GetNthChild(0));
+            thenScope = RegisterScope(myNavigable.GetFirstSingleLayer(Members.Then).GetFirstSingleLayer(Members.Scope));
             //No gaurantees of an else statement
-            if(myNavigable.GetChildCount() > 2 && myNavigable.GetNthChild(2).GetChildCount() != 0)
+            if(myNavigable.GetFirstSingleLayer(Members.Else) != null && myNavigable.GetFirstSingleLayer(Members.Else).GetChildCount() != 0)
             {
-                INavigable scope = myNavigable.GetNthChild(2).GetNthChild(0);//else scope
+                INavigable scope = myNavigable.GetFirstSingleLayer(Members.Else).GetFirstSingleLayer(Members.ElseScope);
                 if (scope.GetChildCount() > 0)
                 {
                     //only else scopes with actual contents are added.
                     //this prevents empty else blocks which are equivalent to the then block above.
                     elseScope = RegisterScope(scope);
                 }
+            }
+            if(myNavigable.GetFirstSingleLayer(MemberSets.Fields) != null)
+            {
+                RegisterFields(myNavigable.GetFirstSingleLayer(MemberSets.Fields));
             }
         }
         #endregion
@@ -66,6 +70,10 @@ namespace SoftwareAnalyzer2.Structure.Graphing.Specifics
             {
                 elseScope.InitialLink();
             }
+            foreach (Field f in AllFields)
+            {
+                f.InitialLink();
+            }
         }
 
         public override void FullLink()
@@ -76,6 +84,10 @@ namespace SoftwareAnalyzer2.Structure.Graphing.Specifics
             if (elseScope != null)
             {
                 elseScope.FullLink();
+            }
+            foreach (Field f in AllFields)
+            {
+                f.FullLink();
             }
         }
         #endregion
@@ -102,6 +114,11 @@ namespace SoftwareAnalyzer2.Structure.Graphing.Specifics
             {
                 elseScope.ExploreGraph(ExploreModes.Full);
             }
+
+            foreach (DefinedMember d in AllFields)
+            {
+                d.ExploreGraph(ExploreModes.Full);
+            }
         }
 
         protected override void WriteEdges()
@@ -113,6 +130,10 @@ namespace SoftwareAnalyzer2.Structure.Graphing.Specifics
             if (elseScope != null)
             {
                 WriteMember(elseScope);
+            }
+            foreach (DefinedMember d in AllFields)
+            {
+                WriteMember(d);
             }
         }
 
---
+++ SQM/test_cpp01/various_cpp_tests/ifStatements/ifStatements.cpp
@@ -0,0 +1,26 @@
+int main()
+{
+    int x = 0;
+    if (x = 1)
+    {
+
+    } else if (x = 2)
+    {
+
+    } else if (x = 3)
+    {
+
+    }
+
+    if (int y = 1)
+    {
+
+    } else if (int y = 2)
+    {
+
+    } else if (int y = 3)
+    {
+
+    }
+    return 0;
+}
